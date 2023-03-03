fc1b4fce4054ae34f73839f95b12cd1682715436
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-03-03 18:57:20 +0000

    tests

--- tree_height.py
+++ tree_height.py
@@ -2,27 +2,28 @@
 import sys
 
 def compute_height(n, parents):
-    # Create a list to represent the tree
-    # Each element of the list will be a list of the indices of the children of the corresponding node
+    # Create a list of empty lists to represent the tree
     tree = [[] for _ in range(n)]
-    # Find the index of the root node
-    root_index = parents.index(-1)
-    # Fill in the children lists of the tree
+    # Find the root node
+    root = None
     for i in range(n):
-        if parents[i] != -1:
+        if parents[i] == -1:
+            root = i
+        else:
+            # Add each node to its parent's list of children
             tree[parents[i]].append(i)
-    # Define a recursive function to compute the height of the tree
-    def get_height(node_index):
+    # Define a helper function to recursively compute the height of a node
+    def height(node):
         # If the node has no children, its height is 1
-        if not tree[node_index]:
+        if not tree[node]:
             return 1
-        # Otherwise, the height of the node is 1 + the maximum height of its children
-        return 1 + max(get_height(child_index) for child_index in tree[node_index])
-    # Compute the height of the tree
-    return get_height(root_index)
+        # Otherwise, its height is the maximum height of its children plus 1
+        return max(height(child) for child in tree[node]) + 1
+    # Compute the height of the root node
+    return height(root)
 
 def main():
-    # Read input
+    # Read input from keyboard
     n = int(input())
     parents = list(map(int, input().split()))
     # Compute and output the height of the tree
@@ -30,6 +31,9 @@ def main():
 
 if __name__ == '__main__':
     # In Python, the default limit on recursion depth is rather low,
-    # so raise it here for this problem.
-    sys.setrecursionlimit(10**7)
-    main()
+    # so raise it here for this problem. Note that to take advantage
+    # of bigger stack, we have to launch the computation in a new thread.
+    sys.setrecursionlimit(10**7)  # max depth of recursion
+    threading.stack_size(2**27)   # new thread will get stack of such size
+    threading.Thread(target=main).start()
+
