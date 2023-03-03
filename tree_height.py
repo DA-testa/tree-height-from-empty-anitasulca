d879d9dbd4211943b413551093ee42aff2ccb4e4
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-03-03 19:00:35 +0000

    tests

--- tree_height.py
+++ tree_height.py
@@ -1,39 +1,41 @@
 #python3
 import sys
+import threading
 
 def compute_height(n, parents):
-    # Create a list of empty lists to represent the tree
-    tree = [[] for _ in range(n)]
-    # Find the root node
-    root = None
+    # Create an array to store the heights of each node
+    heights = [0] * n
+
+    # Iterate through each node, starting from the leaf nodes and moving up the tree
     for i in range(n):
-        if parents[i] == -1:
-            root = i
-        else:
-            # Add each node to its parent's list of children
-            tree[parents[i]].append(i)
-    # Define a helper function to recursively compute the height of a node
-    def height(node):
-        # If the node has no children, its height is 1
-        if not tree[node]:
-            return 1
-        # Otherwise, its height is the maximum height of its children plus 1
-        return max(height(child) for child in tree[node]) + 1
-    # Compute the height of the root node
-    return height(root)
+        # If the node is a leaf, set its height to 1
+        if heights[i] == 0:
+            height = 1
+            node = i
+            while parents[node] != -1:
+                parent = parents[node]
+                if heights[parent] != 0:
+                    height += heights[parent]
+                    break
+                height += 1
+                node = parent
+            heights[i] = height
+
+    # Return the maximum height
+    return max(heights)
 
 def main():
-    # Read input from keyboard
+    # Read input
     n = int(input())
     parents = list(map(int, input().split()))
+
     # Compute and output the height of the tree
     print(compute_height(n, parents))
 
-if __name__ == '__main__':
-    # In Python, the default limit on recursion depth is rather low,
-    # so raise it here for this problem. Note that to take advantage
-    # of bigger stack, we have to launch the computation in a new thread.
-    sys.setrecursionlimit(10**7)  # max depth of recursion
+# In Python, the default limit on recursion depth is rather low,
+# so raise it here for this problem. Note that to take advantage
+# of bigger stack, we have to launch the computation in a new thread.
+sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
 
