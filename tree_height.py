f4d0afdac859dcbfcdf8860dbc2f9e5410ca9ba9
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-03-05 18:51:35 +0000

    TESTS3

--- tree_height.py
+++ tree_height.py
@@ -28,15 +28,15 @@ def compute_height(n, parents):
 def main():
     text = input("Enter 'I' for input from keyboard or 'F' for input from file: ").upper()
 
-    if text[0] == "I":
+    if text == "I":
         n = int(input("Enter number of nodes: "))
         parents_str = input("Enter parent list separated by space: ")
         parents = list(map(int, parents_str.split()))
 
-    elif text[0] == "F":
+    elif text == "F":
         file_name = input("Enter file name: ").strip()
 
-        if "a" in file_name.lower() or "a" in file_name.lower().split("/")[-1]:
+        if "a" in file_name.lower():
             print("File name cannot contain letter 'a'.")
             return
 
@@ -60,3 +60,4 @@ if __name__ == '__main__':
     threading.stack_size(2 ** 27)
     thread = threading.Thread(target=main)
     thread.start()
+
