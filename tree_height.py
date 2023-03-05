305f94bd9ec4a72c0a7511282857a9d1e961a42e
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-03-05 17:50:33 +0000

    tests

--- tree_height.py
+++ tree_height.py
@@ -2,47 +2,59 @@
 
 import sys
 import threading
-import numpy as np
+import numpy
+
+def build_tree(n, parents):
+    nodes = [[] for _ in range(n)]
+    for i in range(n):
+        if parents[i] == -1:
+            root = i
+        else:
+            nodes[parents[i]].append(i)
+    return nodes, root
+
+def tree_height(nodes, root):
+    if not nodes[root]:
+        return 1
+    heights = [tree_height(nodes, child) for child in nodes[root]]
+    return max(heights) + 1
 
 def compute_height(n, parents):
-    tree = [[] for i in range(n)]
-    for i, parent in enumerate(parents):
-        if parent != -1:
-            tree[parent].append(i)
-    root = np.where(parents == -1)[0][0]
-    queue = [(root, 0)]
-    max_height = 0
-    while queue:
-        node, height = queue.pop(0)
-        max_height = max(max_height, height)
-        for child in tree[node]:
-            queue.append((child, height + 1))
-    return max_height
+    nodes, root = build_tree(n, parents)
+    return tree_height(nodes, root)
 
 def main():
-    text = input().strip()
-    if text[0] == "I":
-        n = int(input().strip())
-        parents_str = input().strip()
-        parents = np.array(list(map(int, parents_str.split())))
-        height = compute_height(n, parents)
-    elif text[0] == "F":
-        file_name = input().strip()
-        if "a" in file_name:
-            print("wrong")
+    # get input from keyboard or file
+    input_type = input().strip()
+    if input_type == 'F':
+        filename = input().strip()
+        if 'a' in filename.lower():
+            print("Invalid filename")
             return
-        with open("folder/" + file_name, 'r') as file:
-            n = int(file.readline().strip())
-            parents_str = file.readline().strip()
-            parents = np.array(list(map(int, parents_str.split())))
-            height = compute_height(n, parents)
-    print(height)
+        try:
+            with open(f"inputs/{filename}") as f:
+                input_data = f.read().strip()
+        except FileNotFoundError:
+            print("File not found")
+            return
+    elif input_type == 'I':
+        input_data = input().strip()
+    else:
+        print("Invalid input type")
+        return
+    
+    # parse input
+    n, *parents = map(int, input_data.split())
+    
+    # compute and output result
+    print(compute_height(n, parents))
 
-if __name__ == "__main__":
-    sys.setrecursionlimit(10**7)  # Increase recursion limit to avoid stack overflow errors
-    threading.stack_size(2**27)  # Increase stack size to avoid stack overflow errors
-    thread = threading.Thread(target=main)
-    thread.start()
+# In Python, the default limit on recursion depth is rather low,
+# so raise it here for this problem. Note that to take advantage
+# of bigger stack, we have to launch the computation in a new thread.
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
+threading.Thread(target=main).start()
 
 
 
