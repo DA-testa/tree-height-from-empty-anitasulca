9cab759af8da32cc330d4e26299d03f24c51f912
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-03-05 18:49:19 +0000

    TESTS2

--- tree_height.py
+++ tree_height.py
@@ -4,54 +4,55 @@ import sys
 import threading
 
 def compute_height(n, parents):
-    if n == 1:
-        return 0
-    
-    tree = [[] for i in range(n)]
+    nodes = [[] for _ in range(n)]
     root = None
+
     for i, parent in enumerate(parents):
         if parent == -1:
             root = i
         else:
-            tree[parent].append(i)
+            nodes[parent].append(i)
 
-    heights = [-1] * n
-    heights[root] = 1
-    
-    stack = [root]
+    max_height = 0
+    stack = [(root, 1)]
+
     while stack:
-        node = stack.pop()
-        for child in tree[node]:
-            heights[child] = heights[node] + 1
-            stack.append(child)
-    
-    return max(heights)
+        node, height = stack.pop()
+        max_height = max(max_height, height)
+
+        for child in nodes[node]:
+            stack.append((child, height + 1))
+
+    return max_height
 
 def main():
     text = input("Enter 'I' for input from keyboard or 'F' for input from file: ").upper()
+
     if text[0] == "I":
         n = int(input("Enter number of nodes: "))
         parents_str = input("Enter parent list separated by space: ")
         parents = list(map(int, parents_str.split()))
-        height = compute_height(n, parents)
+
     elif text[0] == "F":
-        file_name = input("Enter file name: ")
+        file_name = input("Enter file name: ").strip()
+
         if "a" in file_name.lower() or "a" in file_name.lower().split("/")[-1]:
             print("File name cannot contain letter 'a'.")
             return
+
         try:
             with open("folder/" + file_name, 'r') as file:
-                n = int(file.readline())
-                parents_str = file.readline().strip()
-                parents = list(map(int, parents_str.split()))
-                height = compute_height(n, parents)
+                n = int(file.readline().strip())
+                parents = list(map(int, file.readline().strip().split()))
         except FileNotFoundError:
             print("File not found.")
             return
+
     else:
         print("Invalid input option.")
         return
 
+    height = compute_height(n, parents)
     print(height)
 
 if __name__ == '__main__':
