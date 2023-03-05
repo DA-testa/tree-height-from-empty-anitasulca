582eda69b53eacca6095bbab5d069394697fde33
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-03-05 17:37:13 +0000

    tests

--- tree_height.py
+++ tree_height.py
@@ -2,47 +2,57 @@
 
 import sys
 import threading
+import numpy as np
 
 def compute_height(n, parents):
-    # Build tree data structure
-    tree = [[] for _ in range(n)]
-    root = -1
-    for i in range(n):
-        parent = parents[i]
-        if parent == -1:
-            root = i
-        else:
+    tree = [[] for i in range (n)]
+    for i, parent in enumerate(parents):
+        if parent !=-1:
             tree[parent].append(i)
-    
-    # Implement memoization
-    memo = {}
+    root = np.where(parents == -1)[0][0]
+    queue = [(root,0)]
+    max_height = 0;
 
-    # Traverse tree to compute height
-    def traverse(node):
-        if node in memo:
-            return memo[node]
-        if not tree[node]:
-            return 1
-        height = max(traverse(child) for child in tree[node]) + 1
-        memo[node] = height
-        return height
-
-    return traverse(root)
+    while queue:
+        node, height = queue.pop(0)
+        max_height = max(max_height, height)
+        for child in tree[node]:
+            queue.append((child, height+1))
+    return max_height+1
 
 def main():
-    # Read input
-    n = int(input())
-    parents = list(map(int, input().split()))
-
-    # Compute height of tree
-    height = compute_height(n, parents)
-
-    # Output result
+    text = input("Enter 'I' for input from keyboard or 'F' for input from file: ")
+    if text[0] == "I":
+        n = int(input("Enter number of nodes: "))
+        parents_str = input("Enter parent list separated by space: ")
+        parents = np.array(list(map(int, parents_str.split())))
+        height = compute_height(n, parents)
+    elif text[0] == "F":
+        file_name = input("Enter file name: ")
+        if "a" in file_name:
+            print("File name cannot contain letter 'a'.")
+            return
+        try:
+            with open("folder/" + file_name, 'r') as file:
+                n = int(file.readline())
+                parents_str = file.readline().strip()
+                parents = np.array(list(map(int, parents_str.split())))
+                height = compute_height(n, parents)
+        except FileNotFoundError:
+            print("File not found.")
+            return
+    else:
+        print("Invalid input option.")
+        return
     print(height)
 
-# Increase recursion limit and stack size
-sys.setrecursionlimit(10**7)
-threading.stack_size(2**27)
+if __name__ == '__main__':
+    sys.setrecursionlimit(10**7)
+    threading.stack_size(2**27)
+    thread = threading.Thread(target=main)
+    thread.start()
 
-# Start main function in a new thread
-threading.Thread(target=main).start()
+
+    
+
+
