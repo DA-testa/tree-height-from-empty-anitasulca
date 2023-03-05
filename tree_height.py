1b2e3766f38e6dabcb173f4bce2f422140a0e99b
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-03-05 18:38:37 +0000

    tests

--- tree_height.py
+++ tree_height.py
@@ -3,46 +3,46 @@
 import sys
 import threading
 
+
 def compute_height(n, parents):
-    # create a list of lists to represent the tree
     tree = [[] for i in range(n)]
     root = None
-    # populate the tree with parent-child relationships
     for i, parent in enumerate(parents):
         if parent == -1:
             root = i
         else:
             tree[parent].append(i)
-    # start with the root node and its height 0
-    queue = [(root, 0)]
+
+    stack = [(root, 0)]
     max_height = 0
-    # traverse the tree breadth-first and update the maximum height
-    while queue:
-        node, height = queue.pop(0)
+
+    while stack:
+        node, height = stack.pop()
         max_height = max(max_height, height)
         for child in tree[node]:
-            queue.append((child, height + 1))
+            stack.append((child, height + 1))
+
     return max_height
 
+
 def main():
-    # ask the user for input from keyboard or file
     text = input("Enter 'I' for input from keyboard or 'F' for input from file: ")
     if text[0] == "I":
-        # read input from keyboard
         n = int(input("Enter number of nodes: "))
         parents_str = input("Enter parent list separated by space: ")
         parents = list(map(int, parents_str.split()))
+        height = compute_height(n, parents)
     elif text[0] == "F":
-        # read input from file
         file_name = input("Enter file name: ")
         if "a" in file_name:
             print("File name cannot contain letter 'a'.")
             return
         try:
             with open("folder/" + file_name, 'r') as file:
-                n = int(file.readline())
+                n = int(file.readline().strip())
                 parents_str = file.readline().strip()
                 parents = list(map(int, parents_str.split()))
+                height = compute_height(n, parents)
         except FileNotFoundError:
             print("File not found.")
             return
@@ -50,12 +50,10 @@ def main():
         print("Invalid input option.")
         return
 
-    # compute the height of the tree and print it
-    height = compute_height(n, parents)
     print(height)
 
+
 if __name__ == '__main__':
-    # increase the recursion limit and stack size to avoid errors
     sys.setrecursionlimit(10 ** 7)
     threading.stack_size(2 ** 27)
     thread = threading.Thread(target=main)
